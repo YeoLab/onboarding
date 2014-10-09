8455a05c0f4ba9e0df04304925f519fd932eef10
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-10-09 13:16:38 -0700

    removing docs from deploy dir

--- docs/sphinx_deployment.mk
+++ docs/sphinx_deployment.mk
@@ -170,8 +170,8 @@ prepare_gh_pages_deployment:
 	echo "Preparing gh_pages deployment..."
 	echo "Pulling any updates from Github Pages..."
 	cd $(DEPLOY_DIR); git pull;
-	mkdir -p $(DEPLOY_DIR)/$(DEPLOY_HTML_DIR)
-	echo "Copying files from '$(BUILDDIR)/html/.' to '$(DEPLOY_DIR)/$(DEPLOY_HTML_DIR)'"
+	mkdir -p $(DEPLOY_DIR)/
+	echo "Copying files from '$(BUILDDIR)/html/.' to '$(DEPLOY_DIR)/'"
 	cp -r $(BUILDDIR)/html/. $(DEPLOY_DIR)/
 
 deploy_gh_pages: prepare_gh_pages_deployment
